2a443614a165eed866430f2fbce190f26829fd07
ankit thakur <ankitsinghthakur1992@email.com>
2026-09-01 16:10:20 +0100

    snapshot

--- models/base/stg_contracts.sql
+++ models/base/stg_contracts.sql
@@ -1,3 +1,10 @@
+{{ config(materialized='incremental', 
+					  incremental_strategy='microbatch',
+					  begin='2026-08-09',
+					  event_time='date', 
+					  batch_size='day',
+                      lookback=2, 
+					  concurrent_batches=false)}}
 
 select
 address,
